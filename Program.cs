d4bea131ebbbc02b37ed12d132cc6335bfbbeeaa
Nurdaulet Batyrbek <nurdamarket@gmail.com>
2024-06-06 02:02:10 +0500

    We add new files C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;  // Подключенное пространство имен
+
+namespace SkillFacotryLessonCsharp.FirstApp  // Объявление пространства имен для данного кода
+{ 
+
+class Program // Объявление класса Program
+    {
+        static void Main(string[] args) // Объявление метода Main
+        {
+            Console.WriteLine("Как вас зовут?"); // Действия внутри метода
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?"); // Действия внутри метода
+            string age = Console.ReadLine();
+            Console.WriteLine("Чем вы занимаетесь?"); // Действия внутри метода
+            string hobby = Console.ReadLine();
+
+            Console.WriteLine("Здравствуйте , {0}", name);
+            Console.WriteLine("Мне сообщили что вам {0}  лет", age);
+            Console.WriteLine("Наверное, {0} - очень интересное занятие. Мне стоит попробовать", hobby);
+            Console.WriteLine("Рад был познакомиться!");
+
+        }
+    }
+}
